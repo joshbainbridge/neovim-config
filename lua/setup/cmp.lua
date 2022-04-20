ad8cd26f82b2b4361a6208d52a99f8d5f3c7badf
Josh Bainbridge <josh.bainbridge@gmail.com>
2022-04-20 10:08:08 +0100

    Update mapping for nvim-cmp

--- lua/setup/cmp.lua
+++ lua/setup/cmp.lua
@@ -1,15 +1,38 @@
+-- Local reference to plugin
+local cmp = require('cmp')
+
 -- Setup nvim-cmp for auto-completion
-require('cmp').setup({
+cmp.setup({
   snippet = {
     expand = function(args)
       require('snippy').expand_snippet(args.body)
     end,
   },
-  mapping = require('cmp').mapping.preset.insert({
-    ['<CR>'] = require('cmp').mapping.confirm({
-	  select = false
-    }),
+  completion = {
+    autocomplete = false,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-n>'] = function(fallback)
+      if not cmp.visible() then
+        cmp.complete()
+      else
+        cmp.select_next_item()
+      end
+    end,
+    ['<C-p>'] = function(fallback)
+      if not cmp.visible() then
+        cmp.complete()
+      else
+        cmp.select_prev_item()
+      end
+    end,
+    ['<CR>'] = cmp.mapping.confirm(),
   }),
+  matching = {
+    disallow_fuzzy_matching = true,
+    disallow_partial_matching = true,
+    disallow_prefix_unmatching = true,
+  },
   sources = {
     { name = 'nvim_lsp', group_index = 1 },
     { name = 'buffer', group_index = 2 },
@@ -19,8 +42,5 @@ require('cmp').setup({
       mode = 'symbol',
       maxwidth = 60,
     })
-  },
-  experimental = {
-    ghost_text = true,
   },
 })
