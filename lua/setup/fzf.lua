916b1ba9ece859708c31cf7dbc01924885d7a3e1
Josh Bainbridge <josh.bainbridge@gmail.com>
2022-12-20 17:37:39 +0000

    Update fzf and add fzf.vim

--- lua/setup/fzf.lua
+++ lua/setup/fzf.lua
@@ -1,5 +1,6 @@
 -- Mapping for fuzzy search with fzf
-vim.keymap.set('n', '<C-P>', '<cmd>FZF<CR>')
+vim.keymap.set('n', '<C-P>', '<cmd>Files<CR>')
+vim.keymap.set('n', '<C-F>', '<cmd>Rg<CR>')
 
 -- Change fzf layout to bottom of screen
 vim.g.fzf_layout = { window = '15new' }
